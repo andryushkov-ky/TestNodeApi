27d32b67e2c12d24118c4b20df08a55c3ea92e7d
Andryushkov-ky <ka@madrobots.ru>
2017-09-07 12:12:32 +0700

    [master] add new api route DELETE /todos/:id

---
+++ playground/mongodb-remove.js
@@ -0,0 +1,9 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+// Todo.findOneAndRemove
+// Todo.remove
+// Todo.findByIdAndRemove
+
--- server/server.js
+++ server/server.js
@@ -49,9 +49,29 @@ app.get('/todos/:id', (req, res) => {
 
             res.send({todo});
         })
-        .catch((e) => {
+        .catch(() => {
             res.status(400).send();
         });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id)
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.send({todo});
+        })
+        .catch(() => {
+            res.status(400).send();
+        })
 });
 
 app.listen(port, () => {
