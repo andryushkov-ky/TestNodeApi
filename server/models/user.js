53df7e497315b86294390cd74e650ad4a487bf54
Andryushkov-ky <ka@madrobots.ru>
2017-09-19 12:27:57 +0700

    [master] update config

--- server/models/user.js
+++ server/models/user.js
@@ -70,7 +70,7 @@ UserSchema.statics.findByToken = function (token) {
     let decoded;
 
     try {
-        decoded = jwt.verify(token, 'secretcode');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {
         // return new Promise((resolve, reject) => {
         //     reject();
